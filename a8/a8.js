fd10c96a0701ec83be6e846d2e6d2498a8cd6887
Patrick <patrickdutra100@hotmail.com>
2022-08-14 22:24:17 -0300

    add_file

---
+++ a8/a8.js
@@ -0,0 +1,31 @@
+let preco = parseFloat(prompt("Valor do produto:"));
+let parcelas = parseInt(prompt("Número de parcelas:"));
+let valorParcelas = 0;
+let precoTotal = 0;
+
+if (parcelas == 1) {
+    parcelaTotal = preco - preco * 0.025;
+    console.log("Você pagará R$", parcelaTotal, "no produto.");
+} else if (parcelas >= 2 && parcelas <= 5) {
+    console.log("Você pagará R$", preco, "no produto.");
+} else if (parcelas >= 6 && parcelas <= 10) {
+    precoTotal = preco + preco * 0.06;
+    valorParcelas = precoTotal / parcelas;
+    console.log("Você pagará R$", precoTotal, "no produto.");
+    console.log(
+        "Você pagará ",
+        parcelas,
+        " parcelas de R$",
+        valorParcelas.toFixed(2)
+    );
+} else if (parcelas >= 11 && parcelas <= 15) {
+    precoTotal = preco + preco * 0.13;
+    valorParcelas = precoTotal / parcelas;
+    console.log("Você pagará R$", precoTotal, "no produto.");
+    console.log(
+        "Você pagará ",
+        parcelas,
+        " parcelas de R$",
+        valorParcelas.toFixed(2)
+    );
+}
